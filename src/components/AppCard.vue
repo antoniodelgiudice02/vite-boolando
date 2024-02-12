dcfb29a61d56fdffab1d582a9eb6acb6d402c34b
antoniodelgiudice02 <antodelgiu02@gmail.com>
2024-02-12 20:40:50 +0100

    create card

---
+++ src/components/AppCard.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+
+    props: {
+        brand: String,
+    }
+}
+</script>
+
+<template>
+    <div><span>{{ brand }}</span></div>
+</template>
+
+<style lang="scss" scoped>
+div{
+    background-color: aqua;
+    width: calc(100% / 3 - 10px);
+}
+</style>
